52816737e6bd4e80f02f61647ae2d181b4244106
zhaolinxu <zhaolinxu@sina.cn>
2018-01-29 12:37:00 +0800

    更新

--- time.php
+++ time.php
@@ -1,2 +1,7 @@
 <?php
+header("Content-type: text/html; charset=utf-8"); 
+header("Access-Control-Allow-Origin:http://likexia.gitee.io");
+header("Access-Control-Allow-Origin:https://zhaolinxu.github.io");
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 echo time();
